a5fe996ee83b9bc42b7682cd2d77cd9dc95b644c
Timotej Bernat <tibe3324@colorado.edu>
2023-04-19 17:47:20 -0600

    Updated setup script to reflect collection format

--- setup_collection.py
+++ setup_collection.py
@@ -1,6 +1,6 @@
 # Custom Imports
 from polymer_utils.general import timestamp_now
-from polymer_utils.logutils import config_mlf_handler
+from polymer_utils.logutils import MultiStreamFileHandler
 from polymer_utils.filetree import startfile
 
 from polymer_utils.representation import PolymerDirManager
@@ -23,39 +23,37 @@ COMPAT_PDB_PATH = Path('compatible_pdbs')
 
 if __name__ == '__main__':
     # Configure paths and parameters
-    reset      = True
-    purge_sims = False 
-    purge_logs = True
+    reset      = False
+    purge_sims = False
+    purge_logs = False
 
-    poly_source_path = COMPAT_PDB_PATH / 'testing'
-    structure_path   = poly_source_path / 'testing_structures'
-    monomer_path     = poly_source_path / 'testing_monomers'
-    collection_path  = COLL_PATH / poly_source_path.name
+    poly_source_path = COMPAT_PDB_PATH / 'water_soluble_Colina'
     solv_template    = RESOURCE_PATH/'inp_templates'/'solv_polymer_template_box.inp'
-
     desired_solvents = (WATER_TIP3P,) # (None,)
     exclusion = 1.0*nanometer
 
-    # Create manager and set up directories, logging all
+    # Define derived paths and create manager
+    collection_path  = COLL_PATH / poly_source_path.name
+    structure_path   = poly_source_path / f'{poly_source_path.name}_structures'
+    monomer_path     = poly_source_path / f'{poly_source_path.name}_monomers'
+
     mgr = PolymerDirManager(collection_path)
 
-    if purge_logs: # imperative that this be done BEFORE the multi-file logger is created (would clear new log otherwise)
+    # Perform manager setup / purge actions
+    if purge_logs: # NOTE : must be done BEFORE log FileHandler is created, as this will destroy it's output as well
         mgr.purge_logs(really=True)
 
     creation_logger = logging.getLogger('polymer_setup')
-    loggers = [creation_logger, polylogger] 
-    creation_log_handler = config_mlf_handler(mgr.log_dir/f'Setup_{timestamp_now()}.log', loggers, writemode='a')
+    logfile_path = mgr.log_dir/f'Setup_{timestamp_now()}.log'
 
-    if reset:
-        mgr.purge_collection(really=True) 
+    with MultiStreamFileHandler(logfile_path, loggers=[creation_logger, polylogger], proc_name=f'Creation of collection "{mgr.collection_dir.name}"'):
+        if reset:
+            mgr.purge_collection(really=True, purge_logs=False) # Explicitly DON'T purge logs here (will be done prior to entering log loop)
 
-    if purge_sims:
-        mgr.purge_sims(really=True)
+        if purge_sims:
+            mgr.purge_sims(really=True)
 
-    if not mgr.mol_dirs: # will be empty if not yet instantiated or if reset prior
-        mgr.populate_collection(struct_dir=structure_path, monomer_dir=monomer_path)
-        mgr.solvate_collection(desired_solvents, template_path=solv_template, exclusion=exclusion)
-        creation_logger.info(f'Completed creation of collection "{collection_path.name}"')
-        
-    creation_log_handler.remove_from_loggers(*loggers)
+        if not mgr.mol_dirs: # will be empty if not yet instantiated or if reset prior
+            mgr.populate_collection(struct_dir=structure_path, monomer_dir=monomer_path)
+            mgr.solvate_collection(desired_solvents, template_path=solv_template, exclusion=exclusion)
     startfile(mgr.collection_dir)
