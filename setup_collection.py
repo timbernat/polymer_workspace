3d75b0748132bb58080b930effe54b46127ac20c
Timotej Bernat <tibe3324@colorado.edu>
2023-04-26 18:06:51 -0600

    Updated collection setup script for formatting

--- setup_collection.py
+++ setup_collection.py
@@ -1,10 +1,11 @@
+'''For initializing a managed collection of Polymers from directories of structure and monomer files'''
+
 # Custom Imports
+from polysaccharide import LOGGERS_MASTER
 from polysaccharide.general import timestamp_now
 from polysaccharide.logutils import MultiStreamFileHandler
 from polysaccharide.filetree import startfile
-
 from polysaccharide.representation import PolymerManager
-from polysaccharide.representation import LOGGER as polylogger
 from polysaccharide.solvation.solvents import WATER_TIP3P
 
 # General Imports
@@ -17,21 +18,23 @@ from openmm.unit import nanometer
 # Static Paths
 RESOURCE_PATH = Path('resources')
 COLL_PATH = Path('Collections')
-COMPAT_PDB_PATH = Path('compatible_pdbs')
+COMPAT_PDB_PATH = Path('compatible_pdbs_updated')
 
 # ------------------------------------------------------------------------------
 
+# Set parameters here
+reset      = True
+purge_sims = True 
+purge_logs = True
+
+# poly_source_path = COMPAT_PDB_PATH / 'simple_polymers'
+# poly_source_path = COMPAT_PDB_PATH / 'water_soluble_polymers'
+poly_source_path = COMPAT_PDB_PATH / 'water_soluble_small'
+solv_template    = RESOURCE_PATH/'inp_templates'/'solv_polymer_template_box.inp'
+desired_solvents = (WATER_TIP3P,) # (None,)
+exclusion = 1.0*nanometer
+
 if __name__ == '__main__':
-    # Configure paths and parameters
-    reset      = False
-    purge_sims = False
-    purge_logs = False
-
-    poly_source_path = COMPAT_PDB_PATH / 'water_soluble_Colina'
-    solv_template    = RESOURCE_PATH/'inp_templates'/'solv_polymer_template_box.inp'
-    desired_solvents = (WATER_TIP3P,) # (None,)
-    exclusion = 1.0*nanometer
-
     # Define derived paths and create manager
     collection_path  = COLL_PATH / poly_source_path.name
     structure_path   = poly_source_path / f'{poly_source_path.name}_structures'
@@ -44,9 +47,10 @@ if __name__ == '__main__':
         mgr.purge_logs(really=True)
 
     creation_logger = logging.getLogger('polymer_setup')
+    loggers = [creation_logger, *LOGGERS_MASTER]
     logfile_path = mgr.log_dir/f'Setup_{timestamp_now()}.log'
 
-    with MultiStreamFileHandler(logfile_path, loggers=[creation_logger, polylogger], proc_name=f'Creation of collection "{mgr.collection_dir.name}"'):
+    with MultiStreamFileHandler(logfile_path, loggers=loggers, proc_name=f'Creation of collection "{mgr.collection_dir.name}"'):
         if reset:
             mgr.purge_collection(really=True, purge_logs=False) # Explicitly DON'T purge logs here (will be done prior to entering log loop)
 
@@ -56,4 +60,3 @@ if __name__ == '__main__':
         if not mgr.polymers: # will be empty if not yet instantiated or if reset prior
             mgr.populate_collection(struct_dir=structure_path, monomer_dir=monomer_path)
             mgr.solvate_collection(desired_solvents, template_path=solv_template, exclusion=exclusion)
-    startfile(mgr.collection_dir)
