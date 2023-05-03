cda7ba959af4fec7dfb6dd5b1760de16a0e02494
Timotej Bernat <tibe3324@colorado.edu>
2023-05-02 19:09:30 -0600

    Removed traj analysis from simulation script

--- run_openmm_sims.py
+++ run_openmm_sims.py
@@ -8,6 +8,7 @@ from openmm.unit import nanometer
 # Logging
 import logging
 logging.basicConfig(level=logging.INFO)
+import matplotlib.pyplot as plt
 
 from polysaccharide import LOGGERS_MASTER
 from polysaccharide.logutils import ProcessLogHandler
@@ -20,7 +21,7 @@ from polysaccharide.logutils import ProcessLogHandler
 from polysaccharide.representation import Polymer, PolymerManager
 from polysaccharide.solvation.solvents import WATER_TIP3P
 from polysaccharide.simulation import SimulationParameters, SimulationPaths
-from polysaccharide.analysis import trajectory, plotprops
+from polysaccharide.analysis import trajectory, plotprops, polyprops
 
 # Static Paths
 COMPAT_PDB_PATH = Path('compatible_pdbs_updated')
@@ -29,41 +30,6 @@ COLL_PATH = Path('Collections')
 RESOURCE_PATH = Path('resources')
 CHG_PARAM_PATH = RESOURCE_PATH / 'chg_templates'
 SIM_PARAM_PATH = RESOURCE_PATH / 'sim_templates'
-
-# ------------------------------------------------------------------------------
-
-def perform_prop_analysis(polymer : Polymer, main_logger : logging.Logger, traj_sample_interval : int, plot : bool=False):
-    '''Analyze trajectories to obtain polymer property data'''
-    # aqcuire files for all information
-    sim_folder = polymer.newest_sim_dir
-    main_logger.info(f'Acquiring trajectory data from {sim_folder}')
-    sim_paths = SimulationPaths.from_file(next(sim_folder.glob('*_paths.json')))
-
-    sim_params = SimulationParameters.from_file(sim_paths.sim_params)
-    state_data = pd.read_csv(sim_paths.state_data)
-    traj = trajectory.load_traj(sim_paths.trajectory, topo_path=polymer.structure_file, sample_interval=traj_sample_interval, remove_solvent=True)
-
-    # save and plot RDF data
-    main_logger.info('Determining pairwise radial distribution functions')
-    rdf_dataframe = trajectory.acquire_rdfs(traj, max_rad=1.0*nanometer)
-    rdf_dataframe.to_csv(sim_folder/'rdfs.csv')
-    if plot:
-        rdf_fig, rdf_ax = plotprops.plot_rdfs(rdf_dataframe, scale=15.0)
-        rdf_fig.suptitle(f'Pairwise Radial Distribution Functions - {polymer.mol_name}')
-        rdf_fig.savefig(sim_folder/f'RDFs.png', bbox_inches='tight')
-    plt.close()
-
-    # save and plot property data
-    main_logger.info('Determining polymer shape properties')
-    prop_dataframe = trajectory.acquire_time_props(traj, properties=analysis.polyprops.DEFAULT_PROPS, time_points=sim_params.time_points[::traj_sample_interval]) 
-    prop_dataframe.to_csv(sim_folder/'time_series.csv')
-    if plot:
-        prop_fig, prop_ax = plotprops.plot_time_props(prop_dataframe, scale=18.0)
-        prop_fig.suptitle(f'Polymer Shape Properties - {polymer.mol_name}')
-        prop_fig.savefig(sim_folder/f'shape_props.png', bbox_inches='tight')
-    plt.close()
-    
-    main_logger.info('Successfully exported trajectory analysis data and plots')
 
 # ------------------------------------------------------------------------------
 
@@ -97,4 +63,3 @@ for sim_param_path in sim_param_paths:
             main_logger.info(f'Current molecule: "{polymer.mol_name}" ({i + 1}/{len(sample_dirs)})') # +1 converts to more human-readable 1-index for step count
             with msf_handler.subhandler(filedir=polymer.logs, loggers=loggers, proc_name=proc_name, timestamp=True) as subhandler: # also log actions to individual Polymers
                 sim_folder = polymer.run_simulation_NPT(sim_params)
-                perform_prop_analysis(polymer, main_logger)
