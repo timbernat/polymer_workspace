4dbacf22e9f7e76d3214c0875d8e4420d01c7f44
Timotej Bernat <tibe3324@colorado.edu>
2023-05-09 16:54:54 -0600

    Grouped figures by molecule, enabled plot overlay

--- generate_figures.py
+++ generate_figures.py
@@ -4,6 +4,7 @@
 import re
 from pathlib import Path
 from collections import defaultdict
+from itertools import combinations
 
 # Numeric Imports
 from math import ceil
@@ -39,16 +40,14 @@ SIM_PARAM_PATH = RESOURCE_PATH / 'sim_templates'
 
 src_coll_path = COLL_PATH / 'water_soluble_large'
 save_dir = Path('figures_for_paper')
+compare_dir = Path('colina_data')
 
 # ------------------------------------------------------------------------------
 
+# load manager and loggers
 mgr = PolymerManager(src_coll_path)
 main_logger = logging.getLogger(__name__)
 loggers = [main_logger, *LOGGERS_MASTER] # loggers from all modules which produce logging output
-
-save_dir.mkdir(exist_ok=True)
-heatmap_dir = save_dir / 'heatmaps'
-heatmap_dir.mkdir(exist_ok=True)
 
 # filter for polymers with simulation and charges successfully generated
 sim_poly = mgr.filtered_by(has_sims)
@@ -58,37 +57,39 @@ chg_methods = set(
             for charge_method in polymer.charges.keys() 
 )
 
-# create workspaces for each charge method
-observ_sets, chg_dirs = {}, {}
-for chg_method in chg_methods:
-    observ_sets[chg_method] = defaultdict(defaultdict) # to accumulate averaged observables
-    
-    chg_dir = save_dir / chg_method # to save plots and data to
-    chg_dir.mkdir(exist_ok=True)
-    chg_dirs[chg_method] = chg_dir
+# create save folder and sets of structure converters and labels
+save_dir.mkdir(exist_ok=True)
+CHG_CVTRS = {
+    'Charge heatmap 2D' : 'SMARTS',
+    'Charge heatmap 3D' : 'CXSMARTS'
+}
 
 # iterate through simulated polymers and generate data and plots
 for mol_name, polymer in sim_poly.items():
+    # create folders to save to
+    parent_dir = save_dir / mol_name
+    parent_dir.mkdir(exist_ok=True)
+    observ = defaultdict(defaultdict)
+
+    fig_time, ax_time = None, None
+    fig_acf, ax_acf = None, None
+
+    # Plot relative charges
+    for chg_pair in combinations(chg_methods, 2): # Plott all possible combinations of pairwise charge comparisons
+        for label, cvtr in CHG_CVTRS.items():
+            fig_chg, ax_chg = polymer.compare_charges(*sorted(chg_pair), converter=cvtr)
+            fig_chg.savefig(parent_dir / f'{label} - {mol_name}', bbox_inches='tight')
+            plt.close()
+        
+    # Plot observables
     for sim_dir, sim_paths_file in polymer.simulation_paths.items():
         sim_paths = SimulationPaths.from_file(sim_paths_file)
         if sim_paths.trajectory.suffix == '.dcd':
             # read simulation trajectory data
             sim_params = SimulationParameters.from_file(sim_paths.sim_params)
+            chg_method = sim_params.charge_method
             time_data = pd.read_csv(sim_paths.time_data)
             spatial_data = pd.read_csv(sim_paths.spatial_data)
-
-            # create folders to save to
-            parent_dir = chg_dirs[sim_params.charge_method] / mol_name
-            parent_dir.mkdir(exist_ok=True)
-
-            # Plot relative charges
-            fig_chg_2d, ax_chg_2d = polymer.compare_charges('ABE10_averaged', 'Espaloma_AM1BCC', converter='SMARTS')
-            fig_chg_2d.savefig(heatmap_dir / f'Charge heatmap 2D - {mol_name}', bbox_inches='tight')
-            plt.close()
-            
-            fig_chg_3d, ax_chg_3d = polymer.compare_charges('ABE10_averaged', 'Espaloma_AM1BCC', converter='CXSMARTS')
-            fig_chg_3d.savefig(heatmap_dir / f'Charge heatmap 3D - {mol_name}', bbox_inches='tight')
-            plt.close()
 
             # Plot RDFs
             radii, rdfs = trajectory.rdfs_to_plot_data(spatial_data)
@@ -99,20 +100,23 @@ for mol_name, polymer in sim_poly.items():
             fig_rdf.savefig(parent_dir / f'RDFs - {mol_name}.png', bbox_inches='tight')
             plt.close()
 
-            # For storing a table of averaged observables
-            observ = defaultdict(defaultdict)
-
             # Plot time series' and autocorrelation functions
             times, time_series = trajectory.props_to_plot_data(time_data)
-            fig_time, ax_time = plotutils.plot_df_props(times, time_series)
-            fig_time.savefig(parent_dir / f'Polymer Properties - {mol_name}.png', bbox_inches='tight')
-            plt.close()
+            if ax_time is None: # create new axes for first time...
+                fig_time, ax_time = plotutils.plot_df_props(times, time_series)
+            else: # otherwise, overlay plots
+                for ax, (prop_name, data) in zip(ax_time, time_series.items()):
+                    ax.plot(times, data)
+                    ax.legend(chg_methods)
 
             time_series_acfs = time_series.apply(statistics.autocorrelate)
             time_series_acfs = time_series_acfs.rename(mapper=lambda name : re.sub('\(.*?\)', 'ACF', name), axis='columns')
-            fig_acf, ax_acf = plotutils.plot_df_props(times, time_series_acfs)
-            fig_acf.savefig(parent_dir / f'Property ACFs - {mol_name}.png', bbox_inches='tight')
-            plt.close()
+            if ax_acf is None: # create new axes for first time...
+                fig_acf, ax_acf = plotutils.plot_df_props(times, time_series_acfs)
+            else: # otherwise, overlay plots
+                for ax, (prop_name, data) in zip(ax_acf, time_series_acfs.items()):
+                    ax.plot(times, data)
+                    ax.legend(chg_methods)
 
             # Determine equilibration times and average observables
             nrows, ncols = 1, len(time_series.columns)
@@ -122,19 +126,30 @@ for mol_name, polymer in sim_poly.items():
                 equil_idx = statistics.equil_loc(series)
                 equil_time = times.to_numpy().flatten()[equil_idx]
                 obs = round(np.mean(series[equil_idx:]), 3)
-                observ_sets[sim_params.charge_method][polymer.base_mol_name][prop_name] = obs
+                observ[f'OpenFF 2.0.0 - {chg_method}'][prop_name] = obs
 
                 ax.plot(times, series)
                 ax.axvline(equil_time, color='r')
                 ax.set_xlabel(times.columns[0])
                 ax.set_ylabel(prop_name)
                 ax.legend([f'<O> = {obs} (after t={equil_time})'], handlelength=0, loc='best') # position annotation in empty region
-            fig_equil.savefig(parent_dir / f'Equilibration times - {mol_name}.png', bbox_inches='tight')
+            fig_equil.savefig(parent_dir / f'Equil times {chg_method} - {mol_name}.png', bbox_inches='tight')
             plt.close()
             plt.close()
 
-# save tables of observables
-for chg_method, observ in observ_sets.items():
-    chg_dir = chg_dirs[chg_method]
+    # Plot overlaid property and ACF data
+    fig_time.legend()
+    fig_time.savefig(parent_dir / f'Polymer Properties - {mol_name}.png', bbox_inches='tight')
+    plt.close()
+    
+    fig_acf.legend()
+    fig_acf.savefig(parent_dir / f'Property ACFs - {mol_name}.png', bbox_inches='tight')
+    plt.close()
+
+    # Save CSV tables of observables, with experimental data appended
+    compare_path = compare_dir / f'{mol_name}_exp.csv'
     observ_df = pd.DataFrame(observ)
-    observ_df.to_csv(chg_dir / f'Observables - {chg_method}.csv')
+    compare_df = pd.read_csv(compare_path, index_col=0) # gets rid of unnecessary index col, enables smooth concatenation
+
+    observ_df_aug = pd.concat([observ_df, compare_df], axis=1)
+    observ_df_aug.to_csv(parent_dir/ f'Observables - {mol_name}.csv')
