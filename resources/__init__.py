137e82988980006ac6f492e3bd23cc07ead1e977
Timotej Bernat <tibe3324@colorado.edu>
2023-05-22 18:14:57 -0600

    Added autoimport and asset lookup to resources

--- resources/__init__.py
+++ resources/__init__.py
@@ -1,1 +1,22 @@
-from . import chg_templates, sim_templates, inp_templates, force_fields
+from pathlib import Path
+from typing import Iterable
+
+import pkgutil, importlib
+import importlib.resources as impres
+import logging
+LOGGER = logging.getLogger(__name__)
+
+def non_dunder(path : Path) -> Iterable[str]:
+    '''Return all subpaths of a directory path which contain no double underscores'''
+    assert(path.is_dir())
+    return [file.name 
+        for file in path.iterdir()
+            if '__' not in file.name
+    ]
+
+AVAIL_RESOURCES = {} # load submodules, record available file assets
+for _loader, _module_name, _ispkg in pkgutil.iter_modules(__path__):
+    module = importlib.import_module(f'{__package__}.{_module_name}')
+    globals()[_module_name] = module # register module to namespace
+    AVAIL_RESOURCES[_module_name] = non_dunder(impres.files(module))
+
