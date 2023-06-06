7139db18747666831603c3b52c7833db8d193334
Timotej Bernat <tibe3324@colorado.edu>
2023-06-06 16:25:43 -0600

    Updated calls to setup_collection.py

--- rct_reduction.sh
+++ rct_reduction.sh
@@ -1,3 +1,3 @@
-python setup_collection.py       -src water_soluble_polymers --coll_name wasps_full
-python create_reduced_structs.py -src wasps_full --output_name water_soluble_reduced -N 200
-python setup_collection.py       -src water_soluble_reduced --coll_name wasps_reduced
+python setup_collection.py       -strct water_soluble_polymers --output_name wasps_full
+python create_reduced_structs.py -src   wasps_full             --output_name water_soluble_reduced -N 200
+python setup_collection.py       -strct water_soluble_reduced  --output_name wasps_reduced
