1ff42b6cea726a3e986e8227310bd58a94ac72e9
Timotej Bernat <tibe3324@colorado.edu>
2023-06-08 17:08:48 -0600

    Modified bash calls to fit script revision

--- rct_reduction.sh
+++ rct_reduction.sh
@@ -1,3 +1,3 @@
 python setup_collection.py       -strct water_soluble_polymers --output_name wasps_full
-python reduce_structs_linear.py  -src   wasps_full             --output_name water_soluble_reduced -N 200 -f paam_modified peg_modified
+python reduce_structs_linear.py  -src   wasps_full           --struct_output water_soluble_reduced -N 200 -f paam_modified peg_modified
 python setup_collection.py       -strct water_soluble_reduced  --output_name wasps_reduced
--- rct_transfer.sh
+++ rct_transfer.sh
@@ -1,3 +1,3 @@
-python transfer_charges.py       -src wasps_reduced -out wasps_full -pdb water_soluble_polymers
+python transfer_monomers.py      -src wasps_reduced -targ wasps_full
 python assign_partial_charges.py -src wasps_full -cp long_chain_chg_params -s unsolv
 python vacuum_anneal_conf.py     -src wasps_full -sim vacuum_anneal -r 3
