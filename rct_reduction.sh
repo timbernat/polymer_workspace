e4be88fee10d4e06024f17783e7f34a33e03cb99
Timotej Bernat <tibe3324@colorado.edu>
2023-06-12 17:04:13 -0600

    Updated RCT shell scripts to component format

--- rct_reduction.sh
+++ rct_reduction.sh
@@ -1,3 +1,5 @@
-python setup_collection.py       -strct water_soluble_polymers --output_name wasps_full
-python reduce_structs_linear.py  -src   wasps_full           --struct_output water_soluble_reduced -N 200 -f paam_modified peg_modified
-python setup_collection.py       -strct water_soluble_reduced  --output_name wasps_reduced
+redux_name='water_soluble_reduced'
+
+python -m setup_collection -strct water_soluble_polymers --output_name wasps_full
+python -m polymer_master -src Collections/wasps_full redux -pdb "compatible_pdbs_updated/$redux_name/${redux_name}_structures" -mono "compatible_pdbs_updated/$redux_name/${redux_name}_monomers" -N 200 -f paam_modified peg_modified
+python -m setup_collection -strct "$redux_name"  --output_name wasps_reduced
--- rct_transfer.sh
+++ rct_transfer.sh
@@ -1,3 +1,6 @@
-python transfer_monomers.py      -src wasps_reduced -targ wasps_full
-python assign_partial_charges.py -src wasps_full -cp long_chain_chg_params -s unsolv
-python vacuum_anneal_conf.py     -src wasps_full -sim vacuum_anneal -r 3
+full_coll="Collections/wasps_full"
+redux_coll="Collection/wasps_reduced"
+
+python -m polymer_master -src $redux_coll transfer -targ $full_coll
+python -m polymer_master -src $full_coll  charge   -cp long_chain_chg_params --no-solvent
+python -m polymer_master -src $full_coll  anneal   -sim vacuum_anneal -r 3
