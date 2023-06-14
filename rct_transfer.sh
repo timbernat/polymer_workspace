e6031e95fb452a28be4f02c77270e462265f7b7a
Timotej Bernat <tibe3324@login12.rc.colorado.edu>
2023-06-14 15:25:17 -0600

    Updated collection names+dir in transfer script

--- rct_transfer.sh
+++ rct_transfer.sh
@@ -1,6 +1,7 @@
-full_coll="Collections/wasps_full"
-redux_coll="Collections/wasps_reduced"
+coll_dir='Collections'
+full_coll="water_soluble_polymers"
+redux_coll="water_soluble_reduced"
 
-python -m polymer_master -src $redux_coll transfer -targ $full_coll
-python -m polymer_master -src $full_coll  charge   -cp long_chain_chg_params --no-solvent
-python -m polymer_master -src $full_coll  anneal   -sim vacuum_anneal -r 3
+python -m polymer_master -src "$coll_dir/$redux_coll" transfer -targ "$coll_dir/$full_coll"
+python -m polymer_master -src "$coll_dir/$full_coll"  charge   -cp long_chain_chg_params --no-solvent
+python -m polymer_master -src "$coll_dir/$full_coll"  anneal   -sim vacuum_anneal -r 3
