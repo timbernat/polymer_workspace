2c650e6e41b865491841b02e9a88dcd2d4da3121
Timotej Bernat <tibe3324@login12.rc.colorado.edu>
2023-06-15 13:10:01 -0600

    Added conformer charge reassignment and solvation to transfer script

--- rct_transfer.sh
+++ rct_transfer.sh
@@ -1,7 +1,11 @@
 coll_dir='Collections'
-full_coll="water_soluble_polymers"
-redux_coll="water_soluble_reduced"
+full_coll="wasps_full"
+redux_coll="wasps_reduced"
+# full_coll="water_soluble_polymers"
+# redux_coll="water_soluble_reduced"
 
 python -m polymer_master -src "$coll_dir/$redux_coll" transfer -targ "$coll_dir/$full_coll"
 python -m polymer_master -src "$coll_dir/$full_coll"  charge   -cp long_chain_chg_params --no-solvent
 python -m polymer_master -src "$coll_dir/$full_coll"  anneal   -sim vacuum_anneal -r 3
+python -m polymer_master -src "$coll_dir/$full_coll"  solvate  --solvents WATER_TIP3P
+python -m polymer_master -src "$coll_dir/$full_coll"  charge   -cp long_chain_chg_params --no-solvent --no-charge # temporary measure to ensure conformers also have charges assigned
