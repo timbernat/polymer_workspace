e04b43b995850263a439073fb71602ab433dd6f1
Timotej Bernat <tibe3324@colorado.edu>
2023-06-16 10:55:09 -0600

    Added conformer generation loop to vacuum_anneal procedure

--- vacuum_anneal.sh
+++ vacuum_anneal.sh
@@ -1,10 +1,21 @@
+#!/bin/bash
+
 coll_dir='Collections'
-full_coll="wasps_full"
+full_name="wasps_full"
+conf_name="${full_name}_confs"
 # full_coll="water_soluble_polymers"
+full_path="$coll_dir/$full_name"
+conf_path="$coll_dir/$conf_name"
 
-python -m setup_collection --output_path "$coll_dir/${full_name}_confs" # create empty destination Collection
-python -m polymer_master -src "$coll_dir/$full_coll"  simulate -sp vacuum_anneal
-python -m polymer_master -src "$coll_dir/$full_coll"  transfer_struct -sp vacuum_anneal
+num_confs=4
 
-python -m polymer_master -src "$coll_dir/$full_coll"  solvate --solvents WATER_TIP3P
-python -m polymer_master -src "$coll_dir/$full_coll"  charge  -cp long_chain_chg_params --no-solvent --no-charge # temporary measure to ensure conformers also have charges assigned
+python -m setup_collection --output_path "$conf_path" # create empty destination Collection
+for ((i=1; i <= $num_confs; i++));
+do
+    echo $i;
+    python -m polymer_master -src "$full_path"  simulate -sp vacuum_anneal
+    python -m polymer_master -src "$full_path"  transfer_struct -dest $conf_path -caf "conf_$i"
+done
+
+python -m polymer_master -src "$conf_path"  solvate --solvents WATER_TIP3P
+python -m polymer_master -src "$conf_path"  charge  -cp long_chain_chg_params --no-solvent --no-charge # temporary measure to ensure conformers also have charges assigned
