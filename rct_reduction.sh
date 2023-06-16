6774775aa27e7e3ef858a195e02657fdbe912647
Timotej Bernat <tibe3324@login13.rc.colorado.edu>
2023-06-16 17:15:51 -0600

    Reconfigured file names to be arg-passable

--- rct_reduction.sh
+++ rct_reduction.sh
@@ -1,8 +1,8 @@
 pdb_dir='compatible_pdbs_updated'
-coll_dir='Collections'
 
-full_name='water_soluble_polymers'
-redux_name='water_soluble_reduced'
+coll_dir=$1
+full_name=$2
+redux_name=$3
 
 python -m setup_collection --output_path "$coll_dir/$full_name"  -pdb "$pdb_dir/$full_name/${full_name}_structures"   -mono "$pdb_dir/$full_name/${full_name}_monomers" 
 python -m polymer_master   -src "$coll_dir/$full_name" redux     -pdb "$pdb_dir/$redux_name/${redux_name}_structures" -mono "$pdb_dir/$redux_name/${redux_name}_monomers" -N 200 -f paam_modified peg_modified
--- rct_transfer.sh
+++ rct_transfer.sh
@@ -1,8 +1,6 @@
-coll_dir='Collections'
-full_coll="wasps_full"
-redux_coll="wasps_reduced"
-# full_coll="water_soluble_polymers"
-# redux_coll="water_soluble_reduced"
+coll_dir=$1
+full_name=$2
+redux_name=$3
 
-python -m polymer_master -src "$coll_dir/$redux_coll" transfer_mono -targ "$coll_dir/$full_coll"
-python -m polymer_master -src "$coll_dir/$full_coll"  charge        -cp long_chain_chg_params --no-solvent
+python -m polymer_master -src "$coll_dir/$redux_name" transfer_mono -targ "$coll_dir/$full_name"
+python -m polymer_master -src "$coll_dir/$full_name"  charge        -cp long_chain_chg_params --no-solvent
