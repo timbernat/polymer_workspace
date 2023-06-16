21f78daa9d410ee113574da7fb17a8c65fb5df68
Timotej Bernat <tibe3324@login13.rc.colorado.edu>
2023-06-16 11:12:20 -0600

    Removed quotes from bash interpolation

--- vacuum_anneal.sh
+++ vacuum_anneal.sh
@@ -9,13 +9,13 @@ conf_path="$coll_dir/$conf_name"
 
 num_confs=4
 
-python -m setup_collection --output_path "$conf_path" # create empty destination Collection
+python -m setup_collection --output_path $conf_path # create empty destination Collection
 for ((i=1; i <= $num_confs; i++));
 do
     echo $i;
-    python -m polymer_master -src "$full_path"  simulate -sp vacuum_anneal
-    python -m polymer_master -src "$full_path"  transfer_struct -dest $conf_path -caf "conf_$i"
+    python -m polymer_master -src $full_path  simulate -sp vacuum_anneal
+    python -m polymer_master -src $full_path  transfer_struct -dest $conf_path -caf conf_$i
 done
 
-python -m polymer_master -src "$conf_path"  solvate --solvents WATER_TIP3P
-python -m polymer_master -src "$conf_path"  charge  -cp long_chain_chg_params --no-solvent --no-charge # temporary measure to ensure conformers also have charges assigned
+python -m polymer_master -src $conf_path  solvate --solvents WATER_TIP3P
+python -m polymer_master -src $conf_path  charge  -cp long_chain_chg_params --no-solvent --no-charge # temporary measure to ensure conformers also have charges assigned
