d16600da99d543a5bb8804ad0698e9d4ca4476fa
Timotej Bernat <tibe3324@login12.rc.colorado.edu>
2023-06-19 17:00:08 -0600

    Added argument passing

--- vacuum_anneal.sh
+++ vacuum_anneal.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-coll_dir='Collections'
-full_name="wasps_full"
-conf_name="${full_name}_confs"
-# full_coll="water_soluble_polymers"
+# arg passing
+coll_dir=$1 # 'Collections'
+full_name=$2 # "wasps_full"
+conf_name=$3
+num_confs=$4 #4
+
+# arg processing
 full_path="$coll_dir/$full_name"
 conf_path="$coll_dir/$conf_name"
-
-num_confs=4
 
 python -m setup_collection --output_path $conf_path # create empty destination Collection
 for ((i=1; i <= $num_confs; i++));
