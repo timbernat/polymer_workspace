7b929db23f628c36e005d0f9e7b396dfcd149338
Timotej Bernat <tibe3324@login13.rc.colorado.edu>
2023-06-20 12:05:10 -0600

    Removed solvation lines from vacuum anneal

--- vacuum_anneal.sh
+++ vacuum_anneal.sh
@@ -13,10 +13,7 @@ conf_path="$coll_dir/$conf_name"
 python -m setup_collection --output_path $conf_path # create empty destination Collection
 for ((i=1; i <= $num_confs; i++));
 do
-    echo $i;
+    echo "Generating conformer set $i/$num_confs";
     python -m polymer_master -src $full_path  simulate -sp vacuum_anneal
     python -m polymer_master -src $full_path  transfer_struct -dest $conf_path -caf conf_$i
 done
-
-python -m polymer_master -src $conf_path  solvate --solvents WATER_TIP3P
-python -m polymer_master -src $conf_path  charge  -cp long_chain_chg_params --solvent --no-charge # temporary measure to ensure conformers also have charges assigned
