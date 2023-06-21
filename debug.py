529294d85206ecbe7d1dc84417fd6ffcc7144505
Timotej Bernat <tibe3324@colorado.edu>
2023-06-21 17:24:17 -0600

    Debugging Topology load issues after equilibration

--- debug.py
+++ debug.py
@@ -1,17 +1,9 @@
 from pathlib import Path
 
 from polysaccharide.polymer.management import PolymerManager
-from polysaccharide.polymer.building import build_linear_polymer, mbmol_from_mono_smarts
 
 p = Path('Collections')
 
-mgr = PolymerManager(p / 'water_soluble_large')
-pdir = mgr.polymers['peg_modified']
-mono_smarts = pdir.monomer_info.monomers
-
-for name, smarts in mono_smarts.items():
-    print(name)
-    mbmol = mbmol_from_mono_smarts(smarts)
-
-chain = build_linear_polymer(monomer_smarts=mono_smarts, DOP=10)
-print(chain)
+mgr = PolymerManager(p / 'water_soluble_polymers_equil')
+pdir = mgr.polymers['paam_modified_conf_1_solv_water_equil']
+offmol = pdir.offmol
