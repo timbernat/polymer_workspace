0c38c721257e146e4dc88cb87009a42627d986b2
Timotej Bernat <tibe3324@colorado.edu>
2023-06-22 15:36:57 -0600

    Removed debug file, updated gitignore

--- debug.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from polysaccharide.polymer.management import PolymerManager
-
-p = Path('Collections')
-
-mgr = PolymerManager(p / 'water_soluble_polymers_equil')
-pdir = mgr.polymers['paam_modified_conf_1_solv_water_equil']
-offmol = pdir.offmol
