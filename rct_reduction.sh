e84a4ce0c2164dcc81524b0a76c5ed6aa336aedc
Timotej Bernat <tibe3324@login12.rc.colorado.edu>
2023-07-13 10:10:34 -0600

    Added PNIPAAm to term group reversal list for building

--- rct_reduction.sh
+++ rct_reduction.sh
@@ -5,5 +5,5 @@ full_name=$2
 redux_name=$3
 
 python -m setup_collection --output_path "$coll_dir/$full_name"  -pdb "$pdb_dir/$full_name/${full_name}_structures"   -mono "$pdb_dir/$full_name/${full_name}_monomers" 
-python -m polymer_master   -src "$coll_dir/$full_name" redux     -pdb "$pdb_dir/$redux_name/${redux_name}_structures" -mono "$pdb_dir/$redux_name/${redux_name}_monomers" -N 200 -f paam_modified peg_modified
+python -m polymer_master   -src "$coll_dir/$full_name" redux     -pdb "$pdb_dir/$redux_name/${redux_name}_structures" -mono "$pdb_dir/$redux_name/${redux_name}_monomers" -N 200 -f peg_modified paam_modified pnipam_modified
 python -m setup_collection --output_path "$coll_dir/$redux_name" -pdb "$pdb_dir/$redux_name/${redux_name}_structures" -mono "$pdb_dir/$redux_name/${redux_name}_monomers" 
